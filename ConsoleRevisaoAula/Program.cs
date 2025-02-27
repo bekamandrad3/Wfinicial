bb06e2133a50b5c40ef3f660d91cd5713a8886de
karolyne.mandrade <karolyne.mandrade@L130635509W10-1>
2025-02-26 22:18:17 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleRevisaoAula/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.Design;
+
+namespace ConsoleRevisaoAula
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite 1ª nota do Aluno!");
+            double notal = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite a 2ª Nota do Aluno!");
+            double nota2 = Convert.ToDouble(Console.ReadLine());
+
+            double media = (notal + nota2) / 2;
+
+            if (media >= 7)
+            {
+                Console.WriteLine("Aprovado!");
+            }
+
+            else if (media < 5)
+            {
+                Console.WriteLine("Reprovado!");
+            }
+            else
+            {
+                Console.WriteLine("Recuperação!");
+            }
+        }
+    }
+}
