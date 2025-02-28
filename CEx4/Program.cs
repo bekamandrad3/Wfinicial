19bd6b2ee257ca7d3494797ce2d1c15c2dc6a892
KAROLYNE MOREIRA ANDRADE <karolyne.mandrade@senacsp.edu.br>
2025-02-28 19:27:14 -0300

    Ex4 Corringindo para windos forms

---
+++ CEx4/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CEx4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o 1° Número: ");
+            int num1 = int.Parse(Console.ReadLine()!);
+            Console.WriteLine("\nDigite o 2° Número: ");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o 2° Número: ");
+            int num3 = int.Parse(Console.ReadLine());
+
+            int maior = Math.Max(num1, num2);
+            maior = Math.Max(maior, num3);
+
+            Console.WriteLine($"\n0 Numero {maior} é o maior valor!");
+        }
+    }
+}
